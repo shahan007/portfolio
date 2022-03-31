25c7eccfcdf94522e2414c565e051e281da593bb
shahan alam <shahanisright@gmail.com>
2022-03-31 22:05:45 +0800

    disabled anchor tag eslint config

--- frontend_react/src/components/NavigationDots.jsx
+++ frontend_react/src/components/NavigationDots.jsx
@@ -1,13 +1,16 @@
-import React from 'react'
+/* eslint-disable jsx-a11y/control-has-associated-label */
+/* eslint-disable jsx-a11y/anchor-has-content */
+
+import React from 'react';
 
 const NavigationDots = ({ active }) => (
   <div className="app__navigation">
-    {["home", "about", "work", "skills", "testimonials", "contact"].map((navItem, index) => (
+    {['home', 'about', 'work', 'skills', 'testimonial', 'contact'].map((item, index) => (
       <a
-        href={`#${navItem}`}
-        key={navItem + index}
+        href={`#${item}`}
+        key={item + index}
         className="app__navigation-dot"
-        style={active === navItem ? { backgroundColor: '#313BAC' } : {}}
+        style={active === item ? { backgroundColor: '#313BAC' } : {}}
       />
     ))}
   </div>
